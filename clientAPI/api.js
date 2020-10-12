6a3e2ed86472b7c6850289e791c57bae42e80e24
Guivia Moreria <guivia.moreira@platform28.com>
2020-10-12 14:40:38 -0400

    Axios perm test url

--- clientAPI/api.js
+++ clientAPI/api.js
@@ -7,7 +7,7 @@ import axios from 'axios';
 import { AsyncStorage } from 'react-native';
 
 const instance =  axios.create({
-    baseURL: 'http://0f869d95bf03.ngrok.io',
+    baseURL: 'http://1b71836f4703.ngrok.io',
 });
 
 instance.interceptors.request.use(
